235289251c654cad964fb8793fbab632b0c33170
victorperezuni <victor.perez9@alu.uclm.es>
2023-11-29 22:24:08 +0100

    Hecho aboutus y demas y creado aboutuscontroller

---
+++ src/main/java/PantysMelRep/domain/controllers/AboutUsController.java
@@ -0,0 +1,21 @@
+package PantysMelRep.domain.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutUsController {
+    @GetMapping("/AboutUS")
+    public String aboutUs() {
+        return "AboutUS";
+    }
+
+    @GetMapping("/ContactUS")
+    public String contactUs() {
+        return "ContactUS";
+    }
+    @GetMapping("/OurTeam")
+    public String ourTeam() {
+        return "OurTeam";
+    }
+}
