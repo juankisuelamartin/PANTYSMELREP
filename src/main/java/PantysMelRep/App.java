76c09cdc51973c6a744b4f5a6aa19872e04581dd
juankisuelamartin <144780658+juankisuelamartin@users.noreply.github.com>
2023-11-29 12:56:39 +0100

    ADD: dependencies

--- src/main/java/PantysMelRep/App.java
+++ src/main/java/PantysMelRep/App.java
@@ -4,20 +4,10 @@
  * Autor: Pan TyS Mel SA
  */
 package PantysMelRep;
-
-import PantysMelRep.domain.controllers.GestorPrestamos;
-import PantysMelRep.domain.controllers.GestorTitulos;
-import PantysMelRep.domain.entities.Autor;
-import PantysMelRep.domain.entities.Titulo;
 import PantysMelRep.persistencia.AgenteBBDD;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
