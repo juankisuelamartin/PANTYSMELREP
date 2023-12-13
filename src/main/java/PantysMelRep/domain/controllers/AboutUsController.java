ab989a4055dec98346a8e804611fb60304a2ec49
victorperezuni <victor.perez9@alu.uclm.es>
2023-12-13 20:37:20 +0100

    AboutUsController mantenimiento comentarios

--- src/main/java/PantysMelRep/domain/controllers/AboutUsController.java
+++ src/main/java/PantysMelRep/domain/controllers/AboutUsController.java
@@ -3,20 +3,24 @@ package PantysMelRep.domain.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//Definición del controlador AboutUsController
 @Controller
 public class AboutUsController
 {
+    //Mapeo de /AboutUS
     @GetMapping("/AboutUS")
     public String aboutUs()
     {
         return "AboutUS";
     }
 
+    //Mapeo de /ContactUS
     @GetMapping("/ContactUS")
     public String contactUs()
     {
         return "ContactUS";
     }
+    //Mapeo de /OurTeam
     @GetMapping("/OurTeam")
     public String ourTeam()
     {
